525151be10aa29a7fa5ea05e90d1c0d36ec22cfd
sumonpaul55 <sumpaul100@gmail.com>
2024-03-10 01:47:01 +0600

    dynamic value passing throuch reducer action payload

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { decreament, increament } from '../redux/features/counter/CounterSlice'
+import { decreament, decreamentByten, increament, inCreamentByValue } from '../redux/features/counter/CounterSlice'
 
 const Counter = () => {
     const { count } = useSelector((state) => state.counter)
@@ -10,8 +10,10 @@ const Counter = () => {
     return (
         <div className='flex items-center gap-3 mt-10'>
             <button onClick={() => dispatch(increament())} className='font-bold text-4xl size-20 bg-red-200'>+</button>
+            <button onClick={() => dispatch(decreamentByten(10))} className='font-bold text-4xl size-20 bg-red-200'>-10</button>
             <h1 className='font-bold text-2xl'>Value: {count}</h1>
             <button onClick={() => dispatch(decreament())} className='font-bold text-4xl size-20 bg-red-200'>-</button>
+            <button onClick={() => dispatch(inCreamentByValue(5))} className='font-bold text-4xl size-20 bg-red-200'>+ 5</button>
         </div>
     )
 }
--- src/redux/features/counter/CounterSlice.js
+++ src/redux/features/counter/CounterSlice.js
@@ -8,15 +8,23 @@ const counterslice = createSlice({
     name: "counter",
     initialState,
     reducers: { // it should be "reducers""Must have to use "s"
+        decreamentByten: (state, action) => {
+            state.count -= action.payload
+        },
         increament: (state) => {
             state.count = state.count + 1
         },
         decreament: (state) => {
             state.count = state.count - 1
-        }
+        },
+        inCreamentByValue: (state, action) => {
+            state.count = state.count + action.payload;
+        },
+
+
     }
 })
 
-export const { increament, decreament } = counterslice.actions
+export const { increament, decreament, inCreamentByValue, decreamentByten } = counterslice.actions
 
 export default counterslice.reducer // here only reducer
